16c7fd140d6e3d5421d65ee9276ee31fc0787266
Tristan C <torisutansan02@gmail.com>
2023-02-18 09:33:58 -0800

    Testing legacy code using React Framework

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            Testing out legacy code&nbsp;
           </p>
           <div>
             <a
