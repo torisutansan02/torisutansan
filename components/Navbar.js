b7bfd9e72ab59c1f6a86f834e5c1912b419fdfe9
Tristan C <torisutansan02@gmail.com>
2023-05-17 10:22:35 -0700

    Adding components

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,14 +2,14 @@ import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <nav className = "navbar">
+        <div className = "navbar">
             <h1> torisutansan </h1>
             <div className = "links">
                 <Link href = "/"> Home </Link>
-                <Link href = "/about"> About </Link>
+                <Link href = "/about" > About </Link>
                 <Link href = "/socials"> Socials </Link>
             </div>
-        </nav>
+        </div>
     );
 }
 
