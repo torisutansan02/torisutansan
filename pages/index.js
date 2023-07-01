2b28944328dce1ba339f94d722886c21a04c399b
Tristan C <torisutansan02@gmail.com>
2023-07-01 15:06:11 -0700

    Editing pages and styles/

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,41 @@ export default function Home() {
             I post my work and other fun side projects on this website.
           </p>
 
+          <h2 className>
+            What even is this?
+          </h2>
+
+          <p>
+            I am going to use this to hone in on my front-end skills. I am hoping I get better with CSS, html and JavaScript.
+          </p>
+
+          <h3>
+            Purpose
+          </h3>
+
+          <p>
+            Hopefully I can optimize my code and other features with this website. Most of what I reference comes from NetNinja on Youtube.
+          </p>
+
+          <h3>
+            Other Activities
+          </h3>
+
+          <p>
+            I will create a sidebar where you can navigate to some of the things I am doing. 
+            The purpose of this sidebar is to show you my various works or projects.
+            It might also contain some of the hobbies or other things I find of interest.
+          </p>
+
+          <p>
+            Other than that, I hope this becomes an experiment I can have a bit of fun with. 
+            I am using HTML, CSS, JavaScript, Tailwind primarily for this project.
+          </p>
+
+          <p>
+            Please click some of the links below :3
+          </p>
+
         <main>
         <div className = {styles.grid}>
           <a
