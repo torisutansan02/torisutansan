d06499d5834a192c157aa741b3a5f46e3c9a38d0
Tristan C <torisutansan02@gmail.com>
2023-09-25 16:29:59 -0700

    Making necessary css changes

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -18,13 +18,13 @@ function Sidebar() {
   return (
     <div>
       <Disclosure as="nav">
-        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
+        <Disclosure.Button className="mt-10 absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
           <GiHamburgerMenu
             className="block md:hidden h-6 w-6"
             aria-hidden="true"
           />
         </Disclosure.Button>
-        <div className="p-1 w-11 h-screen bg-zinc-800 z-20 fixed top-10 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+        <div className="p-3 w-15 h-screen bg-zinc-800 z-20 fixed top-10 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
           <div className="flex flex-col justify-start item-center">
             <div className=" my-4 border-b border-gray-100 pb-4">
               <Link className="flex mb-2 justify-start items-center gap-1 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto" href = "/">
