e5bcf8eef597f659227e9159c831e016b846ef77
Tristan C <torisutansan02@gmail.com>
2023-09-25 15:28:44 -0700

    adding all files

---
+++ components/Sidebar.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Link from 'next/link'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Disclosure } from "@headlessui/react";
+import {
+  MdOutlineSpaceDashboard,
+  MdOutlineAnalytics,
+  MdOutlineIntegrationInstructions,
+  MdOutlineMoreHoriz,
+  MdOutlineSettings,
+  MdOutlineLogout,
+} from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { FaRegComments } from "react-icons/fa";
+import { BiMessageSquareDots } from "react-icons/bi";
+
+function Sidebar() {
+  return (
+    <div>
+      <Disclosure as="nav">
+        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
+          <GiHamburgerMenu
+            className="block md:hidden h-6 w-6"
+            aria-hidden="true"
+          />
+        </Disclosure.Button>
+        <div className="p-1 w-11 h-screen bg-zinc-800 z-20 fixed top-10 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+          <div className="flex flex-col justify-start item-center">
+            <div className=" my-4 border-b border-gray-100 pb-4">
+              <Link className="flex mb-2 justify-start items-center gap-1 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto" href = "/">
+                <MdOutlineSpaceDashboard className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-white group-hover:text-white font-semibold ">
+                  Home
+                </h3>
+              </Link>
+              <Link className="flex  mb-2 justify-start items-center gap-1 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto" href = "/about">
+                <CgProfile className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-white group-hover:text-white font-semibold ">
+                  About
+                </h3>
+              </Link>
+              <Link className="flex  mb-2 justify-start items-center gap-1 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto" href = "/socials">
+                <FaRegComments className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-white group-hover:text-white font-semibold ">
+                  Social
+                </h3>
+              </Link>
+            </div>
+            {/* setting  */}
+            <div className=" my-4 border-b border-gray-100 pb-4">
+              <div className="flex mb-2 justify-start items-center gap-1 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineSettings className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-white group-hover:text-white font-semibold ">
+                  Settings
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-1 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineMoreHoriz className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-white group-hover:text-white font-semibold ">
+                  More
+                </h3>
+              </div>
+            </div>
+            {/* logout */}
+            {/*
+            <div className=" my-4">
+              <div className="flex mb-2 justify-start items-center gap-21pl-5 border border-gray-200  hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineLogout className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Logout
+                </h3>
+              </div>
+            </div>
+          */}
+          </div>
+        </div>
+      </Disclosure>
+    </div>
+  );
+}
+
+export default Sidebar;
