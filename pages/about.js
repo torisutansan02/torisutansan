083b8015547023550a42318a6c7fc8533ecb811d
Tristan C <torisutansan02@gmail.com>
2023-09-30 12:14:49 -0700

    Adding more to my about page and fixing mobile css

--- pages/about.js
+++ pages/about.js
@@ -14,14 +14,24 @@ function About() {
             <p> 
                 Hello, my name is Tristan. Down below, you will find links to a couple of past
                 projects. The entire purpose of this project is to demonstrate my ability in
-                frontend and CSS. I am using Tailwind CSS to develop this website. In the future,
-                I want to also implement user authentication and a database to turn this into a
-                blog-like website.
+                frontend and CSS. I am using Tailwind CSS to develop this website.
             </p>
 
-            <Link href = "https://rubbishradartristan.web.app" className = "bg-zinc-700 hover:bg-gray-900 p-2 m-2 rounded-md"> Rubbish Radar </Link>
+            <p>
+                My goal in life is to be a happier, more productive individual. I need to find
+                fulfilling and cognitive activities to satisfy this. Therefore, you will often
+                notive how expressive I am.
+            </p>
 
-            <Link href = "https://torisutanproject.vercel.app/" className = "bg-zinc-700 hover:bg-gray-900 p-2 m-2 rounded-md"> Biohack Project </Link>
+            <p>
+                Largely speaking, I try to stick with routines and habits I build on a daily
+                basis. I need to have a routine, not a regiment. This is because I want to
+                adjust for times where life becomes difficult.
+            </p>
+
+            <Link href = "https://rubbishradartristan.web.app" className = "bg-zinc-700 hover:bg-gray-900 p-2 m-2 rounded-md grid w-40 items-center text-center content-center"> Rubbish Radar </Link>
+
+            <Link href = "https://torisutanproject.vercel.app/" className = "bg-zinc-700 hover:bg-gray-900 p-2 m-2 rounded-md grid w-40 items-center text-center content-center"> Biohack Project </Link>
 
             <Footer /> 
         </>
