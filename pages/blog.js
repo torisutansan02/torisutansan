33aa2310817a69b0be5416b63b0624cc322bf8a7
Tristan C <torisutansan02@gmail.com>
2023-09-30 09:55:24 -0700

    Adding server props

---
+++ pages/blog.js
@@ -0,0 +1,36 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Sidebar from '../components/Sidebar'
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Blog({ allPostsData }) {
+  return (
+    <>
+                <Navbar />
+            <Sidebar></Sidebar>
+        <h2>Blog</h2>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+
+        <Footer />
+    </>
+  );
+}
