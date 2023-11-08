228245ffddaf92f222f73238ba547186b9ee9cfe
Tristan C <torisutansan02@gmail.com>
2023-11-08 13:02:00 -0800

    making small change to my about page.

--- pages/about.js
+++ pages/about.js
@@ -12,21 +12,22 @@ function About() {
 
             <h1 className = "heading"> About Me </h1>  
             <p className = "pretty"> 
-                Hello, my name is Tristan. Down below, you will find links to a couple of past
-                projects. The entire purpose of this project is to demonstrate my ability in
-                frontend and CSS. I am using Tailwind CSS to develop this website.
+                My name is Tristan Cai. I identify as non-binary and my pronouns are they/they/theirs.
+                I like to work on various projects. The entire purpose of this website is to prove and
+                demonstrate my skills. I am using Tailwind CSS to develop the front-end of this website.
             </p>
 
             <p className = "pretty">
-                My goal in life is to be a happier, more productive individual. I need to find
-                fulfilling and cognitive activities to satisfy this. Therefore, you will often
-                notive how expressive I am.
+                My goal in life is to be a happier, more productive individual. I find fulfilling 
+                activities to spend thinking about these days. Naturally, I feel happy as an 
+                expressive person.
             </p>
 
             <p className = "pretty">
-                Largely speaking, I try to stick with routines and habits I build on a daily
-                basis. I need to have a routine, not a regiment. This is because I want to
-                adjust for times where life becomes difficult.
+                I try to stick with daily routines and habits. These are mostly dietary and ritualistic.
+                These activities promote health and well-being. Healthy diets, intense physical exercise,
+                and thinking occur naturally. I'm streamlining these processes and enhancing my creative
+                output.
             </p>
 
             <Link href = "https://rubbishradartristan.web.app" className = "bg-zinc-700 hover:bg-gray-900 p-2 m-2 rounded-md flex w-40 text-center justify-center mx-auto text-white" target = "_blank"> Rubbish Radar </Link>
