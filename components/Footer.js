e452bf6cfe93bd8477440ee015693d0f67b5f291
torisutansan02 <torisutansan02@gmail.com>
2025-02-26 17:47:39 -0800

    Adding new libraries

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import styles from '@/styles/Home.module.css'
-import { Inter } from '@next/font/google'
+import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ['latin'] })
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Inter } from '@next/font/google';
+import { Inter } from "next/font/google";
 import Image from 'next/image'; // Import the Image component
 
 import Navbar from '../components/Navbar'
