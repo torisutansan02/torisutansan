615b3d12951d84581b85eac9372912a82492b5ac
torisutansan02 <torisutansan02@gmail.com>
2025-02-27 10:15:57 -0800

    Removing middleware error

--- middleware.js
+++ middleware.js
@@ -2,25 +2,20 @@ import { NextResponse } from "next/server";
 import { auth0 } from "./lib/auth0";
 
 export async function middleware(request) {
-  try {
     const authRes = await auth0.middleware(request);
 
     if (request.nextUrl.pathname.startsWith("/auth")) {
-      return authRes;
+        return authRes;
     }
 
     const { origin } = new URL(request.url)
     const session = await auth0.getSession();
 
     if (!session) {
-      return NextResponse.redirect(`${origin}/auth/login`); // ✅ Fixed for production
+        return NextResponse.redirect(`${origin}/auth/login`); // ✅ Fixed for production
     }
 
     return authRes;
-  } catch (error) {
-    console.error("Middleware error:", error);
-    return NextResponse.error();
-  }
 }
 
 export const config = {
