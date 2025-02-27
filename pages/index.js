939f3231d2f89aa42a92381bfef64742bf099311
torisutansan02 <torisutansan02@gmail.com>
2025-02-27 10:31:05 -0800

    Fixing production

--- pages/index.js
+++ pages/index.js
@@ -8,22 +8,6 @@ import Sidebar from '../components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const mongoose = require('mongoose');
-
-const uri = process.env.MONGODB_URI;
-
-async function connect(){
-  try {
-      await mongoose.connect(uri);
-      console.log("connected to MongoDB");
-  }
-  catch {
-      console.log("error");
-  }
-}
-
-connect();
-
 export default function Home() {
   return (
     <>
